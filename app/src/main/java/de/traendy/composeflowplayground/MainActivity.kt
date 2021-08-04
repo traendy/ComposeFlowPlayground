779338378d760d118acf66ebb023234be6647c5d
Soenke <peters.soenke@gmail.com>
2021-08-04 18:43:10 +0200

    project init

---
+++ app/src/main/java/de/traendy/composeflowplayground/MainActivity.kt
@@ -0,0 +1,38 @@
+package de.traendy.composeflowplayground
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import de.traendy.composeflowplayground.ui.theme.ComposeFlowPlaygroundTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            ComposeFlowPlaygroundTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(color = MaterialTheme.colors.background) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    ComposeFlowPlaygroundTheme {
+        Greeting("Android")
+    }
+}
